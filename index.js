f9e430eee6ef1d5e311dc2679158c64ddc2e8ddc
I-sandesh <83366556+I-Himanshu@users.noreply.github.com>
2022-12-29 19:41:52 +0530

    Modiefied

--- index.js
+++ index.js
@@ -1,45 +1,27 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+async function generateCertificate(USER){
 
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const app = require('express');
-const http = require('http')
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  let html = fs.readFileSync("index.html", "utf-8");
+  html = html.replace("$NAME",USER.name).replace("$ROLE",USER.role).replace("$SCORE",USER.score).replace("$DATE",USER.date);
+  await page.setContent(html, { waitUntil: "domcontentloaded" });
+  await page.emulateMediaType("screen");
+  const pdf = await page.pdf({
+    path: `result_new.pdf`,
+    // margin: { bottom: "20px", top:"20px",right:"20px",'bottom':"20px"},
+    printBackground: false,
+    preferCSSPageSize: true,
+    format: "A4",
+    landscape: true
+  });
 
-
-  (async () => {
-
-    var type = process.argv.slice(2)[0] || 'url';
-
-    const browser = await puppeteer.launch();
-
-    const page = await browser.newPage();
-
-
-    if (type === 'url') {
-
-      const website_url = 'https://vcint.github.io/nodeapi/';
-
-      await page.goto(website_url, { waitUntil: 'networkidle0' });
-
-    } else if (type === 'file') {
-
-      const html = fs.readFileSync('sample.html', 'utf-8');
-      await page.setContent(html, { waitUntil: 'domcontentloaded' });
-
-    } else {
-
-      console.log(new Error(`HTML source "${type}" is unkown.`));
-      await browser.close();
-      return;
-    }
-    await page.emulateMediaType('screen');
-
-    const pdf = await page.pdf({
-      path: `result_${type}.pdf`,
-      margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
-      printBackground: true,
-      format: 'A4',
-    });
-
-    await browser.close();
-    res.send(pdf);
-  })();
+  await browser.close();
+}
+generateCertificate({
+  name: "Himanshu Kumar",
+  role: "Developer",
+  score: "100",
+  date: "12 December 2022"
+})

a0b0dae7aa2d160f2247d32c5d72bff8c0f263cb
I-sandesh <83366556+I-Himanshu@users.noreply.github.com>
2023-01-02 12:12:24 +0530

    Can Download certificate

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
-async function generateCertificate(USER){
+const express = require("express");
+const app = express()
+const port = 3000
 
+async function generateCertificate(USER){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   let html = fs.readFileSync("index.html", "utf-8");
@@ -9,19 +12,31 @@ async function generateCertificate(USER){
   await page.setContent(html, { waitUntil: "domcontentloaded" });
   await page.emulateMediaType("screen");
   const pdf = await page.pdf({
-    path: `result_new.pdf`,
+    // path: `result_new.pdf`,
     // margin: { bottom: "20px", top:"20px",right:"20px",'bottom':"20px"},
     printBackground: false,
     preferCSSPageSize: true,
     format: "A4",
     landscape: true
   });
-
   await browser.close();
+  return pdf;
 }
-generateCertificate({
-  name: "Himanshu Kumar",
-  role: "Developer",
-  score: "100",
-  date: "12 December 2022"
+
+app.get('/', async (req, res) => {
+  // return pdf file 
+  
+  let pdf = await generateCertificate({
+    name: "Himanshu Kumar",
+    role: "Developer",
+    score: Math.random(),
+    date: "12 December 2022"
+  });
+  res.type("pdf")
+  res.send(pdf);
+}
+)
+
+app.listen(port, () => {
+  console.log(`Certificate app listening at http://localhost:${port}`)
 })
